27a47cf039a087b53bee29b2338ae114aa376828
Majulia <majulialcarvalho@gmail.com>
2024-04-06 17:52:27 -0300

    criando arquivo de conexão

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php 
+
+    $conect = new PDO("mysql:dbname=testephp;host=localhost:3306","root", "banana666");
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
 
-    $pdo = new PDO("mysql:dbname=testephp;host=localhost:3306","root", "banana666");
-    $sql = $pdo->query('SELECT * FROM produtos');
+    require_once('conexao.php');
+;
+    $sql = $conect->query('SELECT * FROM produtos');
 
     $dados = $sql -> fetchAll(pdo::FETCH_ASSOC); 
 
     echo'<pre>';
-    print_r($dados);    
+    print_r($dados);   
 
 ?>
